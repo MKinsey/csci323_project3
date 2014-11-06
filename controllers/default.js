bae1a9092dbeb713ab91b01594b060c40acda820
aaroncameron21 <aaroncameron21@gmail.com>
2014-11-05 19:17:40 -0700

    Command system implemented, cleaned up code.

--- controllers/default.js
+++ controllers/default.js
@@ -4,22 +4,98 @@ exports.install = function(framework) {
     framework.websocket('/', socket_homepage, ['json']);
 }
 
-var step = 0;
+var steps = 0;
 var Body = require('../verlet-js/lib/body.js');
 
+// TIMING
 var timer = new Date();
 var frameTime = timer.getTime();            // Instatiate initial timing objects
-var deltaTime = 0;
+var deltaTime = 15;
 var ping = 15;
+var play = false;
 
+var controller;
+
+
+// PHYSICS BODIES
 var bodies = new Array(2);
 bodies[0] = new Body(  0,  0,  0,   0, 1000,0);        // Trivial example
-bodies[1] = new Body( 10,  0,  0, 0.1, 10,1);
+bodies[1] = new Body( 10,  1,  0, 0.1, 10,1);
 
+
+// MESSAGES
+var outPositions = new Array(bodies.length);
+var out = "";
+
+// CONSTANTS
 var G = 0.0006673;      // Establish gravitational constant
 var PI2 = Math.PI * 2;      // Establish PI2 constant
 
-var out = "";
+
+
+function step() {
+    if (play) {
+        steps += 1;
+        console.log("Step: " + steps);
+        controller.send(simulate());
+        setTimeout(step, 17);
+    }
+
+}
+
+function simulate() {
+    for (var a = 0; a < bodies.length; a++) {
+        var bodyA = bodies[a];
+        //console.log(">" + bodies[a].toString());
+        bodyA.resetForce()
+        for (var b = 0; b < bodies.length; b++) {
+            var bodyB = bodies[b];
+            if (bodyA.id != bodyB.id) {
+                var r = getDistance(bodyA.x, bodyA.y, bodyB.x, bodyB.y);
+                var theta = Math.atan((bodyB.y - bodyA.y) / (bodyB.x - bodyA.x));
+                if (bodyB.x < bodyA.x) {
+                    theta += Math.PI;
+                }
+                if (theta >= PI2) {
+                    theta -= PI2;
+                }
+                if (theta < 0) {
+                    theta += PI2;
+                }
+                var tF = G * (bodyA.m * bodyB.m) / r ^ 2;
+                var tFx = Math.cos(theta) * tF;
+                var tFy = Math.sin(theta) * tF;
+                bodyA.addForce(tFx, tFy);
+
+                //console.log("Add Force: From: " + bodyB.toString() + " r: " + r + " | F: " + tF + " Fx: " + tFx + " Fy: " + tFy + " | Deg: " + (theta * (180 / Math.PI)) + " Rad: " + theta);
+
+            }
+        }
+        //console.log(bodies[a].toString());
+    }
+
+    timer = new Date();
+    deltaTime = timer.getTime() - frameTime;
+    ping = ping * 0.9 + deltaTime * 0.1;
+    out = "";
+
+    var box;
+
+    //console.log("deltaTime: " + deltaTime + " Ping: " + ping);
+    if (ping < 25) {
+        for (var c = 0; c < bodies.length; c++) {
+            bodies[c].applyForce(deltaTime / 1000);
+            outPositions[c] = bodies[c].serializeUpdate();
+            out = out + " / " + bodies[c].toString();
+        }
+        box = { command: 'update', positions: outPositions};
+        // send to all without this client
+    }
+    else {box = { command: 'message', message: 'Waiting for ping reduction: ' + ping};}
+    frameTime = timer.getTime();
+    return box;
+
+}
 
 function view_usage() {
     var self = this;
@@ -33,14 +109,22 @@ function view_homepage() {
 
 function socket_homepage() {
 
-    var controller = this;
+    controller = this;
 
     controller.on('open', function(client) {
 
         // WHEN USER CONNECTS
         console.log('Connect / Online:', controller.online);
-        client.send({ message: 'User Connected: {0}'.format(client.id) });
-        controller.send({ message: 'Connect new user: {0}\nOnline: {1}'.format(client.id, controller.online) }, [], [client.id]);
+        client.send({command: 'message', message: 'User Connected: {0}'.format(client.id) });
+        controller.send({command: 'message',  message: 'Connect new user: {0}\nOnline: {1}'.format(client.id, controller.online) }, [], [client.id]);
+
+        var initBodies = new Array(bodies.length);
+        for(var i = 0; i < bodies.length; i++) {
+            initBodies[i] = bodies[i].serializeInitial();
+        }
+
+        var box = {command: 'initialize', bodies: initBodies};
+        controller.send(box);
 
     });
 
@@ -56,66 +140,30 @@ function socket_homepage() {
 
     controller.on('message', function(client, message) {
 
+        var command = message.command;
+        console.log("Command: " + command);
+
+        if (command == 'start') {
+            console.log("Starting...");
+            play = true;
+            step();
+        }
+        if (command == 'stop') {
+            console.log("Stopping...");
+            play = false;
+        }
+        if (command == 'reset') {
+            console.log("Resetting...");
+        }
+
+
 
         if (typeof(message.username) !== 'undefined') {
-            var old = client.id;
-            client.id = message.username;
-            controller.send({ message: 'rename: ' + old + ', new: ' + client.id });
+            //var old = client.id;
+            //client.id = message.username;
+            //controller.send({ message: 'rename: ' + old + ', new: ' + client.id });
             return;
         }
-
-        console.log("Step: " + step)
-
-        for (var a = 0; a < bodies.length; a++) {
-            var bodyA = bodies[a];
-            console.log(">" + bodies[a].toString());
-            bodyA.resetForce()
-            for (var b = 0; b < bodies.length; b++) {
-                var bodyB = bodies[b];
-                if (bodyA.id != bodyB.id) {
-                    var r = getDistance(bodyA.x, bodyA.y, bodyB.x, bodyB.y);
-                    var theta = Math.atan((bodyB.y - bodyA.y) / (bodyB.x - bodyA.x));
-                    if (bodyB.x < bodyA.x) {
-                        theta += Math.PI;
-                    }
-                    if (theta >= PI2) {
-                        theta -= PI2;
-                    }
-                    if (theta < 0) {
-                        theta += PI2;
-                    }
-                    var tF = G * (bodyA.m * bodyB.m) / r ^ 2;
-                    var tFx = Math.cos(theta) * tF;
-                    var tFy = Math.sin(theta) * tF;
-                    bodyA.addForce(tFx, tFy);
-
-                    //console.log("Add Force: From: " + bodyB.toString() + " r: " + r + " | F: " + tF + " Fx: " + tFx + " Fy: " + tFy + " | Deg: " + (theta * (180 / Math.PI)) + " Rad: " + theta);
-
-                }
-            }
-            //console.log(bodies[a].toString());
-        }
-
-        timer = new Date();
-        deltaTime = timer.getTime() - frameTime;
-        ping = ping * 0.9 + deltaTime * 0.1;
-        out = "Ping: " + Math.round(ping) + " | ";
-
-        console.log("deltaTime: " + deltaTime + " Ping: " + ping);
-        if (ping < 50) {
-            for (var c = 0; c < bodies.length; c++) {
-                bodies[c].applyForce(deltaTime / 1000);
-                out = out + bodies[c].serialize();
-            }
-        }
-        frameTime = timer.getTime();
-
-
-
-        // send to all without this client
-        step += 1;
-        message.message = out;
-        controller.send(message);
 
     });
 
@@ -124,6 +172,11 @@ function socket_homepage() {
         framework.error(error, 'websocket', controller.uri);
 
     });
+
+    controller.on('start', function(client, message) {
+        console.log("You just started this bitch!");
+    });
+
 
     // how many connections?
     // controller.online;
--- verlet-js/lib/body.js
+++ verlet-js/lib/body.js
@@ -35,6 +35,7 @@ function Body(x, y, xx, yy, m, id) {
     this.fx = 0;
     this.fy = 0;
     this.m = m;
+    this.c = 0;
     this.id = id;
 }
 
@@ -74,151 +75,160 @@ Body.prototype.applyForce = function(dt,pdt) {
 
 }
 
-Body.prototype.serialize = function() {
-    return this.id + "," + Math.round(this.x*100)/100 + "," + Math.round(this.y*100)/100 + "|"
+Body.prototype.serializeUpdate = function() {
+    return [this.id,Math.round(this.x),Math.round(this.y)];
+    //return this.id + "," + Math.round(this.x*100)/100 + "," + Math.round(this.y*100)/100 + "|"
 }
-
-Body.prototype.add = function(v) {
-	return new Body(this.x + v.x, this.y + v.y);
-}
-
-Body.prototype.sub = function(v) {
-	return new Body(this.x - v.x, this.y - v.y);
-}
-
-Body.prototype.mul = function(v) {
-	return new Body(this.x * v.x, this.y * v.y);
-}
-
-Body.prototype.div = function(v) {
-	return new Body(this.x / v.x, this.y / v.y);
-}
-
-Body.prototype.scale = function(coef) {
-	return new Body(this.x*coef, this.y*coef);
-}
-
-Body.prototype.mutableSet = function(v) {
-	this.x = v.x;
-	this.y = v.y;
-	return this;
-}
-
-Body.prototype.mutableAdd = function(v) {
-	this.x += v.x;
-	this.y += v.y;
-	return this;
-}
-
-Body.prototype.mutableSub = function(v) {
-	this.x -= v.x;
-	this.y -= v.y;
-	return this;
-}
-
-Body.prototype.mutableMul = function(v) {
-	this.x *= v.x;
-	this.y *= v.y;
-	return this;
-}
-
-Body.prototype.mutableDiv = function(v) {
-	this.x /= v.x;
-	this.y /= v.y;
-	return this;
-}
-
-Body.prototype.mutableScale = function(coef) {
-	this.x *= coef;
-	this.y *= coef;
-	return this;
-}
-
-Body.prototype.equals = function(v) {
-	return this.x == v.x && this.y == v.y;
-}
-
-Body.prototype.epsilonEquals = function(v, epsilon) {
-	return Math.abs(this.x - v.x) <= epsilon && Math.abs(this.y - v.y) <= epsilon;
-}
-
-Body.prototype.length = function(v) {
-	return Math.sqrt(this.x*this.x + this.y*this.y);
-}
-
-Body.prototype.length2 = function(v) {
-	return this.x*this.x + this.y*this.y;
-}
-
-Body.prototype.dist = function(v) {
-	return Math.sqrt(this.dist2(v));
-}
-
-Body.prototype.dist2 = function(v) {
-	var x = v.x - this.x;
-	var y = v.y - this.y;
-	return x*x + y*y;
-}
-
-Body.prototype.normal = function() {
-	var m = Math.sqrt(this.x*this.x + this.y*this.y);
-	return new Body(this.x/m, this.y/m);
-}
-
-Body.prototype.dot = function(v) {
-	return this.x*v.x + this.y*v.y;
-}
-
-Body.prototype.angle = function(v) {
-	return Math.atan2(this.x*v.y-this.y*v.x,this.x*v.x+this.y*v.y);
-}
-
-Body.prototype.angle2 = function(vLeft, vRight) {
-	return vLeft.sub(this).angle(vRight.sub(this));
-}
-
-Body.prototype.rotate = function(origin, theta) {
-	var x = this.x - origin.x;
-	var y = this.y - origin.y;
-	return new Body(x*Math.cos(theta) - y*Math.sin(theta) + origin.x, x*Math.sin(theta) + y*Math.cos(theta) + origin.y);
+Body.prototype.serializeInitial = function() {
+    return [this.id,Math.round(this.x),Math.round(this.y),this.m,this.c];
+    //return this.id + "," + Math.round(this.x*100)/100 + "," + Math.round(this.y*100)/100 + "|"
 }
 
 Body.prototype.toString = function() {
-	return "ID: " + this.id + " (" + this.x + ", " + this.y + ") m=" + this.m;
+    return "ID: " + this.id + " (" + this.x + ", " + this.y + ") m=" + this.m;
 }
 
-function test_Body() {
-	var assert = function(label, expression) {
-		console.log("Body(" + label + "): " + (expression == true ? "PASS" : "FAIL"));
-		if (expression != true)
-			throw "assertion failed";
-	};
-	
-	assert("equality", (new Body(5,3).equals(new Body(5,3))));
-	assert("epsilon equality", (new Body(1,2).epsilonEquals(new Body(1.01,2.02), 0.03)));
-	assert("epsilon non-equality", !(new Body(1,2).epsilonEquals(new Body(1.01,2.02), 0.01)));
-	assert("addition", (new Body(1,1)).add(new Body(2, 3)).equals(new Body(3, 4)));
-	assert("subtraction", (new Body(4,3)).sub(new Body(2, 1)).equals(new Body(2, 2)));
-	assert("multiply", (new Body(2,4)).mul(new Body(2, 1)).equals(new Body(4, 4)));
-	assert("divide", (new Body(4,2)).div(new Body(2, 2)).equals(new Body(2, 1)));
-	assert("scale", (new Body(4,3)).scale(2).equals(new Body(8, 6)));
-	assert("mutable set", (new Body(1,1)).mutableSet(new Body(2, 3)).equals(new Body(2, 3)));
-	assert("mutable addition", (new Body(1,1)).mutableAdd(new Body(2, 3)).equals(new Body(3, 4)));
-	assert("mutable subtraction", (new Body(4,3)).mutableSub(new Body(2, 1)).equals(new Body(2, 2)));
-	assert("mutable multiply", (new Body(2,4)).mutableMul(new Body(2, 1)).equals(new Body(4, 4)));
-	assert("mutable divide", (new Body(4,2)).mutableDiv(new Body(2, 2)).equals(new Body(2, 1)));
-	assert("mutable scale", (new Body(4,3)).mutableScale(2).equals(new Body(8, 6)));
-	assert("length", Math.abs((new Body(4,4)).length() - 5.65685) <= 0.00001);
-	assert("length2", (new Body(2,4)).length2() == 20);
-	assert("dist", Math.abs((new Body(2,4)).dist(new Body(3,5)) - 1.4142135) <= 0.000001);
-	assert("dist2", (new Body(2,4)).dist2(new Body(3,5)) == 2);
-
-	var normal = (new Body(2,4)).normal()
-	assert("normal", Math.abs(normal.length() - 1.0) <= 0.00001 && normal.epsilonEquals(new Body(0.4472, 0.89443), 0.0001));
-	assert("dot", (new Body(2,3)).dot(new Body(4,1)) == 11);
-	assert("angle", (new Body(0,-1)).angle(new Body(1,0))*(180/Math.PI) == 90);
-	assert("angle2", (new Body(1,1)).angle2(new Body(1,0), new Body(2,1))*(180/Math.PI) == 90);
-	assert("rotate", (new Body(2,0)).rotate(new Body(1,0), Math.PI/2).equals(new Body(1,1)));
-	assert("toString", (new Body(2,4)) == "(2, 4)");
-}
-
+//Body.prototype.add = function(v) {
+//	return new Body(this.x + v.x, this.y + v.y);
+//}
+//
+//Body.prototype.sub = function(v) {
+//	return new Body(this.x - v.x, this.y - v.y);
+//}
+//
+//Body.prototype.mul = function(v) {
+//	return new Body(this.x * v.x, this.y * v.y);
+//}
+//
+//Body.prototype.div = function(v) {
+//	return new Body(this.x / v.x, this.y / v.y);
+//}
+//
+//Body.prototype.scale = function(coef) {
+//	return new Body(this.x*coef, this.y*coef);
+//}
+//
+//Body.prototype.mutableSet = function(v) {
+//	this.x = v.x;
+//	this.y = v.y;
+//	return this;
+//}
+//
+//Body.prototype.mutableAdd = function(v) {
+//	this.x += v.x;
+//	this.y += v.y;
+//	return this;
+//}
+//
+//Body.prototype.mutableSub = function(v) {
+//	this.x -= v.x;
+//	this.y -= v.y;
+//	return this;
+//}
+//
+//Body.prototype.mutableMul = function(v) {
+//	this.x *= v.x;
+//	this.y *= v.y;
+//	return this;
+//}
+//
+//Body.prototype.mutableDiv = function(v) {
+//	this.x /= v.x;
+//	this.y /= v.y;
+//	return this;
+//}
+//
+//Body.prototype.mutableScale = function(coef) {
+//	this.x *= coef;
+//	this.y *= coef;
+//	return this;
+//}
+//
+//Body.prototype.equals = function(v) {
+//	return this.x == v.x && this.y == v.y;
+//}
+//
+//Body.prototype.epsilonEquals = function(v, epsilon) {
+//	return Math.abs(this.x - v.x) <= epsilon && Math.abs(this.y - v.y) <= epsilon;
+//}
+//
+//Body.prototype.length = function(v) {
+//	return Math.sqrt(this.x*this.x + this.y*this.y);
+//}
+//
+//Body.prototype.length2 = function(v) {
+//	return this.x*this.x + this.y*this.y;
+//}
+//
+//Body.prototype.dist = function(v) {
+//	return Math.sqrt(this.dist2(v));
+//}
+//
+//Body.prototype.dist2 = function(v) {
+//	var x = v.x - this.x;
+//	var y = v.y - this.y;
+//	return x*x + y*y;
+//}
+//
+//Body.prototype.normal = function() {
+//	var m = Math.sqrt(this.x*this.x + this.y*this.y);
+//	return new Body(this.x/m, this.y/m);
+//}
+//
+//Body.prototype.dot = function(v) {
+//	return this.x*v.x + this.y*v.y;
+//}
+//
+//Body.prototype.angle = function(v) {
+//	return Math.atan2(this.x*v.y-this.y*v.x,this.x*v.x+this.y*v.y);
+//}
+//
+//Body.prototype.angle2 = function(vLeft, vRight) {
+//	return vLeft.sub(this).angle(vRight.sub(this));
+//}
+//
+//Body.prototype.rotate = function(origin, theta) {
+//	var x = this.x - origin.x;
+//	var y = this.y - origin.y;
+//	return new Body(x*Math.cos(theta) - y*Math.sin(theta) + origin.x, x*Math.sin(theta) + y*Math.cos(theta) + origin.y);
+//}
+//
+//Body.prototype.toString = function() {
+//	return "ID: " + this.id + " (" + this.x + ", " + this.y + ") m=" + this.m;
+//}
+//
+//function test_Body() {
+//	var assert = function(label, expression) {
+//		console.log("Body(" + label + "): " + (expression == true ? "PASS" : "FAIL"));
+//		if (expression != true)
+//			throw "assertion failed";
+//	};
+//
+//	assert("equality", (new Body(5,3).equals(new Body(5,3))));
+//	assert("epsilon equality", (new Body(1,2).epsilonEquals(new Body(1.01,2.02), 0.03)));
+//	assert("epsilon non-equality", !(new Body(1,2).epsilonEquals(new Body(1.01,2.02), 0.01)));
+//	assert("addition", (new Body(1,1)).add(new Body(2, 3)).equals(new Body(3, 4)));
+//	assert("subtraction", (new Body(4,3)).sub(new Body(2, 1)).equals(new Body(2, 2)));
+//	assert("multiply", (new Body(2,4)).mul(new Body(2, 1)).equals(new Body(4, 4)));
+//	assert("divide", (new Body(4,2)).div(new Body(2, 2)).equals(new Body(2, 1)));
+//	assert("scale", (new Body(4,3)).scale(2).equals(new Body(8, 6)));
+//	assert("mutable set", (new Body(1,1)).mutableSet(new Body(2, 3)).equals(new Body(2, 3)));
+//	assert("mutable addition", (new Body(1,1)).mutableAdd(new Body(2, 3)).equals(new Body(3, 4)));
+//	assert("mutable subtraction", (new Body(4,3)).mutableSub(new Body(2, 1)).equals(new Body(2, 2)));
+//	assert("mutable multiply", (new Body(2,4)).mutableMul(new Body(2, 1)).equals(new Body(4, 4)));
+//	assert("mutable divide", (new Body(4,2)).mutableDiv(new Body(2, 2)).equals(new Body(2, 1)));
+//	assert("mutable scale", (new Body(4,3)).mutableScale(2).equals(new Body(8, 6)));
+//	assert("length", Math.abs((new Body(4,4)).length() - 5.65685) <= 0.00001);
+//	assert("length2", (new Body(2,4)).length2() == 20);
+//	assert("dist", Math.abs((new Body(2,4)).dist(new Body(3,5)) - 1.4142135) <= 0.000001);
+//	assert("dist2", (new Body(2,4)).dist2(new Body(3,5)) == 2);
+//
+//	var normal = (new Body(2,4)).normal()
+//	assert("normal", Math.abs(normal.length() - 1.0) <= 0.00001 && normal.epsilonEquals(new Body(0.4472, 0.89443), 0.0001));
+//	assert("dot", (new Body(2,3)).dot(new Body(4,1)) == 11);
+//	assert("angle", (new Body(0,-1)).angle(new Body(1,0))*(180/Math.PI) == 90);
+//	assert("angle2", (new Body(1,1)).angle2(new Body(1,0), new Body(2,1))*(180/Math.PI) == 90);
+//	assert("rotate", (new Body(2,0)).rotate(new Body(1,0), Math.PI/2).equals(new Body(1,1)));
+//	assert("toString", (new Body(2,4)) == "(2, 4)");
+//}
+//
